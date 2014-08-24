4723bdfc620d8151782ea52daa3c8737403a20b3
Thomas Feng <tfeng@linkedin.com>
2014-08-24 02:18:08 -0700

    Release 0.1.3 to try to fix an issue caused by sbteclipse downgrading sbt version.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 package me.tfeng.play.plugins
 
 object Versions {
-  val project = "0.1.3-SNAPSHOT"
+  val project = "0.1.3"
 
-  val sbtPlugins = "0.1.2"
+  val sbtPlugins = "0.1.3"
 
   val avro = "1.7.7"
 
