007b44362658ee3ca7a6d6192b0aeb70497c1ed8
Thomas Feng <tfeng@linkedin.com>
2014-09-25 01:44:54 -0700

    Release 0.2.1.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,9 +2,9 @@ package me.tfeng.play.plugins
 
 object Versions {
 
-  val project = "0.2.1-SNAPSHOT"
+  val project = "0.2.1"
 
-  val sbtPlugins = "0.2.1-SNAPSHOT"
+  val sbtPlugins = "0.2.1"
 
   val scala = "2.10.4"
 
